c8fd7975243a4c79e031d8637164e5eadc170d47
Joy <kweyuayieko@gmail.com>
2025-08-07 14:48:36 +0300

    edited contacts page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
       <Fade direction="down" triggerOnce>
         <div className="contact-hero">
           <h1>Contact Us</h1>
-          <p>We’d love to hear from you — whether you’re a doctor, student, or just curious.</p>
+          <p>We’d love to hear from you whether you’re a doctor, student, or just curious.</p>
         </div>
       </Fade>
 
@@ -20,7 +20,7 @@ const Contact = () => {
           <p><FaEnvelope className="icon" /> <strong>Email:</strong> mokuamaurine531@gmail.com / kweyuayieko@gmail.com</p>
           <p><FaPhoneAlt className="icon" /> <strong>Phone:</strong> +254 713 554 218 / +254 797 683 168</p>
           <p><FaMapMarkerAlt className="icon" /> <strong>Location:</strong> Nairobi, Kenya</p>
-          <p className="contact-footer">We're just an email or call away — don’t hesitate to say hi!</p>
+          <p className="contact-footer">We're just an email or call away, don’t hesitate to say hi!</p>
         </div>
       </Fade>
     </div>
